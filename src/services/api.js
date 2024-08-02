a218e2dd5e5e5a6f3e13a12b14ed8ef7f29b9ab6
Michael <micaheledquilan@gmail.com>
2024-08-02 19:56:56 -0400

    updated service apis for posts

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const API_URL = "http://localhost:3000/api/v1";
 //change to game and posts
-export const getCats = () => axios.get(`${API_URL}/cats`);
-export const getCatById = (id) => axios.get(`${API_URL}/cat/${id}`);
-export const createCat = (cat) => axios.post(`${API_URL}/cat`, cat);
-export const updateCat = (id, cat) => axios.put(`${API_URL}/cat/${id}`, cat);
-export const deleteCat = (id) => axios.delete(`${API_URL}/cat/${id}`);
+export const getReviews = () => axios.get(`${API_URL}/posts`);
+export const getReviewById = (id) => axios.get(`${API_URL}/post/${id}`);
+export const createReview = (post) => axios.post(`${API_URL}/post`, post);
+export const updateReview = (id, post) =>
+  axios.put(`${API_URL}/post/${id}`, post);
+export const deleteReview = (id) => axios.delete(`${API_URL}/post/${id}`);
